341d4110335f0ce28f0a7e5145f8d98a1bc671f0
Ian Kirsch <ian.kirsch@code.berlin>
2022-03-01 19:59:59 +0100

    Read serial data

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
 
 int main() {
-  printf("Hello world.");
+  int serial_port;
+  char dat;
+  if ((serial_port = serialOpen("/dev/ttyS0", 9600)) < 0) {
+    fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
+    return 1;
+  }
+
+  if (wiringPiSetup() == -1) {
+    fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
+    return 1;
+  }
+
+  while (1) {
+    if (serialDataAvail(serial_port)) {
+      data = serialGetchar(serial_port); /* receive character serially */
+      printf("%c", data);
+      fflush(stdout);
+      // serialPutchar(serial_port, data); /* transmit character serially on port */
+    }
+  }
 }
