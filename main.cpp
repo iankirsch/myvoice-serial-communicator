6a1114ea1b0319d32816b921c0c19db5882a6109
Eric Middelhove <eric.middelhove@icloud.com>
2022-03-03 17:57:24 +0000

    Add welcome message

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 #include "wiringPi/wiringPi.h"
 #include "wiringPi/wiringSerial.h"
 
 int main() {
+  extern void clear();
+
   int serial_port;
   char data;
+
   if ((serial_port = serialOpen("/dev/ttyS0", 9600)) < 0) {
     fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
     return 1;
@@ -17,12 +22,29 @@ int main() {
     return 1;
   }
 
+  // Clear console
+  clear();
+
+  // Print success message
+  printf("Successfully connected to serial device on '/dev/ttyS0'.\nAwaiting data...\n");
+
   while (1) {
     if (serialDataAvail(serial_port)) {
       data = serialGetchar(serial_port); /* receive character serially */
       printf("%c", data);
       fflush(stdout);
-      // serialPutchar(serial_port, data); /* transmit character serially on port */
+      // serialPutchar(serial_port, data); /* transmit character serially on
+      // port */
     }
   }
+}
+
+void clear() {
+#if defined _WIN32
+  system("cls");
+#elif defined(__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+  system("clear");
+#elif defined(__APPLE__)
+  system("clear");
+#endif
 }
